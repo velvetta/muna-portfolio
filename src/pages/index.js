bec91833b5e51008fd5a67fe6de3560761535c07
Edosa Kelvin <edosakelvin@gmail.com>
2019-09-14 20:11:33 +0100

    Small tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import React from "react"
 import style from "../styles/index.module.css"
 
-export default () => <div className={style.test}>Ibobo Art</div>
+export default () => <div>
+    <h1 className={style.header}>Hi! Welcome to my little site</h1>
+    <p className={style.sub}>I hope you like what you find :)</p>
+</div>
